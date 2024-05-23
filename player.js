5fe083e7ed00874c25e10a24fa3cac6278c40d58
AndreyGOIT <ae.borga@gmail.com>
2024-05-23 13:13:49 +0300

    create playlist

---
+++ player.js
@@ -0,0 +1,26 @@
+// data
+const playlist1 = {
+    title: 'Hip-Hop Hits',
+    coverImageUrl: './playlist1.png',
+    info: {
+        totalTrackCount: 4,
+        totalTrackDurationInSeconds: 772
+    },
+    tracks: [
+        {
+            coverImageUrl: './track1.png',
+            artistName: 'Eminem',
+            title: 'Rap God',
+            fileUrl: 'Eminem_-_Rap_God.mp3',
+            isHot: false
+        },
+        {
+            coverImageUrl: './track2.png',
+            artistName: '50 Cent',
+            title: 'In da Club',
+            fileUrl: '50_Cent_-_In_Da_Club_FlexyOkay.com.mp3',
+            isHot: true
+        }
+    ]
+  }
+  // render
