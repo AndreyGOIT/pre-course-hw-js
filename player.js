7348c7a5f95719549cdd888f437496ba09d97d7b
AndreyGOIT <ae.borga@gmail.com>
2024-05-23 13:33:19 +0300

    add tracks

--- player.js
+++ player.js
@@ -1,33 +1,50 @@
 // data
 const playlist1 = {
-    title: 'Hip-Hop Hits',
-    coverImageUrl: './playlist1.png',
-    info: {
-        totalTrackCount: 4,
-        totalTrackDurationInSeconds: 772
+  title: "Hip-Hop Hits",
+  coverImageUrl: "./playlist1.png",
+  info: {
+    totalTrackCount: 4,
+    totalTrackDurationInSeconds: 772,
+  },
+  tracks: [
+    {
+      coverImageUrl: "./track1.png",
+      artistName: "Eminem",
+      title: "Rap God",
+      fileUrl: "Eminem_-_Rap_God.mp3",
+      isHot: false,
     },
-    tracks: [
-        {
-            coverImageUrl: './track1.png',
-            artistName: 'Eminem',
-            title: 'Rap God',
-            fileUrl: 'Eminem_-_Rap_God.mp3',
-            isHot: false
-        },
-        {
-            coverImageUrl: './track2.png',
-            artistName: '50 Cent',
-            title: 'In da Club',
-            fileUrl: '50_Cent_-_In_Da_Club_FlexyOkay.com.mp3',
-            isHot: true
-        }
-    ]
-  }
-  // render
-let playlistTitleElement = document.createElement('h1');
+    {
+      coverImageUrl: "./track2.png",
+      artistName: "50 Cent",
+      title: "In da Club",
+      fileUrl: "50_Cent_-_In_Da_Club_FlexyOkay.com.mp3",
+      isHot: true,
+    },
+  ],
+};
+// render
+let playlistImageElement = document.createElement("img");
+playlistImageElement.src = playlist1.coverImageUrl;
+document.body.append(playlistImageElement);
+
+let playlistTitleElement = document.createElement("h1");
 playlistTitleElement.append(playlist1.title);
 document.body.append(playlistTitleElement);
 
-let playlistImageElement = document.createElement('img');
-playlistImageElement.src = playlist1.coverImageUrl;
-document.body.append(playlistImageElement);
+let trackListElement = document.createElement("ul");
+
+let track1Element = document.createElement("li");
+track1Element.append(
+  playlist1.tracks[0].artistName + " - " + playlist1.tracks[0].title
+);
+
+let track2Element = document.createElement("li");
+track2Element.append(
+  playlist1.tracks[1].artistName + " - " + playlist1.tracks[1].title
+);
+
+trackListElement.append(track1Element);
+trackListElement.append(track2Element);
+
+document.body.append(trackListElement);
