042b4fec8ca4080da6f68b523959769265ed231d
AndreyGOIT <ae.borga@gmail.com>
2024-05-30 11:18:36 +0300

    copy of object #5

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,7 +2,8 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-const passport2 = structuredClone(passport);
+// const passport2 = structuredClone(passport);
+const passport2 = Object.assign({}, passport);
 passport2.name = "Ivan";
 
 console.log(passport);
